54feb93cf99545fc644b41dc94b20963477af086
Ankur Bag <ankur.bag@sap.com>
2018-06-16 16:18:58 -0400

    Added cors changes

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,17 +1,14 @@
 import axios from 'axios';
-// import cors from 'cors';
+import {API_CALL} from '../utils/project_consts';
 
 export const  CREATE_USER ='CREATE_USER';
 export const LOGIN_USER ='LOGIN_USER';
 export const FETCH_POST ='FETCH_POST';
 
-
-
 export function createUser(values){
   return(dispatch, getState) =>{
     debugger
-  const request = axios.post('http://deframe-prod-api.us-east-1.elasticbeanstalk.com:80/accounts/register',
-  values)
+  const request = axios.post(API_CALL+'/accounts/register',values)
 .then((response) => {
        dispatch({
          type: CREATE_USER,
@@ -25,7 +22,7 @@ export function loginUser(values){
 
   
   return(dispatch,getState) =>{
-  const request = axios.post('http://deframe-prod-api.us-east-1.elasticbeanstalk.com:80/accounts/login', values)
+  const request = axios.post(API_CALL+'/accounts/login', values)
   return(dispatch) =>{
     request.then(()=>{
       dispatch({
@@ -39,7 +36,7 @@ export function loginUser(values){
 
 
 export function fetchPost(id){ //we will make sure props we pass are title categories
-  const request = axios.get('http://deframe-prod-api.us-east-1.elasticbeanstalk.com:80/users/id/1')
+  const request = axios.get(API_CALL+'/users/id/1')
   return(dispatch) =>{
     request.then(()=>{
 dispatch({
