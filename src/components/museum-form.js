1e446ab26c8cb5f04b4d0f1a5c8abc9d1defc074
RasyadiAbdoellah <rfabdoellah@gmail.com>
2019-04-06 11:53:07 -0400

    add all fields for museum

--- src/components/museum-form.js
+++ src/components/museum-form.js
@@ -29,6 +29,22 @@ class AddNewMuseum extends Component{
         <div className="container">
           <div className="row">
             <div className="col">
+            {/* {
+              "acronym": "string",
+              "bannerUrl": "string",
+              "city": "string",
+              "country": "string",
+              "id": 0,
+              "lat": 0,
+              "lng": 0,
+              "logoUrl": "string",
+              "name": "string",
+              "priceDetails": "string",
+              "state": "string",
+              "street": "string",
+              "timingDetails": "string",
+              "zip": "string"
+            } */}
                 <h2> Museum Information </h2>
                 <form onSubmit ={ handleSubmit(this.onSubmit.bind(this))}>
                   <Field
@@ -48,6 +64,68 @@ class AddNewMuseum extends Component{
                     component={this.renderField}
                     type="text"
                     placeholder="Museum information"
+                  />
+                  <Field
+                    name="timingDetails"
+                    component={this.renderField}
+                    type="text"
+                    placeholder="Museum times open"
+                  />
+                  <h3>Address</h3>
+                  <Field
+                    name = "street"
+                    component= {this.renderField}
+                    type="text"
+                    placeholder = "Address"
+                  />
+                  <Field
+                    name = "city"
+                    component= {this.renderField}
+                    type="text"
+                    placeholder = "City"
+                  />
+                  <Field
+                    name = "state"
+                    component= {this.renderField}
+                    type="text"
+                    placeholder = "State"
+                  />
+                  <Field
+                    name = "zip"
+                    component= {this.renderField}
+                    type="text"
+                    placeholder = "ZIP"
+                  />
+                  <Field
+                    name = "country"
+                    component= {this.renderField}
+                    type="text"
+                    placeholder = "Country"
+                  />
+                  <Field
+                    name = "lat"
+                    component= {this.renderField}
+                    type="text"
+                    placeholder = "Latitude"
+                  />
+                  <Field
+                    name = "long"
+                    component= {this.renderField}
+                    type="text"
+                    placeholder = "Longitude"
+                  />
+                  <h3>Media</h3>
+                  <Field
+                    name="bannerUrl"
+                    component={this.renderField}
+                    type="text"
+                    placeholder="Banner image URL"
+                  />
+                  <Field
+                    name="logoUrl"
+                    component={this.renderField}
+                    type="text"
+                    placeholder="Logo URL"
                   />
                   <button type="submit" className ="mt-5 btn form-btn">Save</button>
                   <button type="reset" className ="mt-5 btn form-btn">Cancel</button>
